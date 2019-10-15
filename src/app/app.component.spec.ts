7bde4c38dbe0189a496a1364f7d6417c6e99b2e2
Maria Uyeda <mariauyeda@Marias-MBP.attlocal.net>
2019-10-15 10:26:27 -0700

    Fixes existing tests and adds new app tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,11 +1,23 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { BrowserModule, By } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+
+import { DoctorService } from './doctor.service';
+
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [
+        BrowserModule,
+        HttpClientModule
+      ],
       declarations: [
         AppComponent
       ],
+      providers: [
+        DoctorService
+      ]
     }).compileComponents();
   }));
   it('should create the app', async(() => {
@@ -13,15 +25,27 @@ describe('AppComponent', () => {
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
-  it(`should have as title 'app'`, async(() => {
+  it(`should have as title 'Sharp Code Challenge'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app');
+    expect(app.title).toEqual('Sharp Code Challenge');
   }));
   it('should render title in a h1 tag', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to app!');
+    expect(compiled.querySelector('h1').textContent).toContain('Sharp Code Challenge');
+  }));
+  it(`should render 'Family Practice Doctors' subtitle`, async(() => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.debugElement.query(By.css('.family-list'));
+    expect(compiled.nativeElement.textContent).toContain('Family Practice Doctors');
+  }));
+  it(`should render 'Pediatric Doctors' subtitle`, async(() => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.debugElement.query(By.css('.pediatric-list'));
+    expect(compiled.nativeElement.textContent).toContain('Pediatric Doctors');
   }));
 });
--- src/app/doctor.service.spec.ts
+++ src/app/doctor.service.spec.ts
@@ -1,11 +1,14 @@
 import { TestBed, inject } from '@angular/core/testing';
-
+import { HttpClientModule } from '@angular/common/http';
 import { DoctorService } from './doctor.service';
 
 describe('DoctorService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [DoctorService]
+      providers: [DoctorService],
+      imports: [
+        HttpClientModule
+      ]
     });
   });
 
